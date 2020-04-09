f73708d8cf2eb475d1d7e672897f9c357a50a072
Ania Dalecka <gapiontko@o2.pl>
2020-04-09 14:01:30 +0200

    Fix remove class 'active' from all articles

--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
 const titleClickHandler = function(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
   console.log(event);
 
-  /* Remove class 'active' from all article links  */
+  /* [Done] Remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
 
-  /* Add class 'active' to the clicked link */
+  /* [In progress] Add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
 
-  /* Remove class 'active' from all articles */
-  const activeArticle = document.querySelectorAll('.posts article.active');
+  /* [Done] Remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts article.active');
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
+  }
 
   /* Get 'href' attribute from the clicked link */
 
