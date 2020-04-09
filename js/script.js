bbb5840ee7f6c272a3a2f6a6e7b0c3c1092f74d1
Ania Dalecka <gapiontko@o2.pl>
2020-04-09 13:43:09 +0200

    Remove class 'active' from all articles

--- js/script.js
+++ js/script.js
@@ -2,21 +2,26 @@ const titleClickHandler = function(event){
   console.log('Link was clicked!');
   console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* Remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-  /* add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+  /* Add class 'active' to the clicked link */
 
-  /* get 'href' attribute from the clicked link */
+  /* Remove class 'active' from all articles */
+  const activeArticle = document.querySelectorAll('.posts article.active');
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
 
-  /* add class 'active' to the correct article */
+  /* Get 'href' attribute from the clicked link */
+
+  /* Find the correct article using the selector (value of 'href' attribute) */
+
+  /* Add class 'active' to the correct article */
 }
 
 const links = document.querySelectorAll('.titles a');
